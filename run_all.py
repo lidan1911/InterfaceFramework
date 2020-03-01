7315b782b969113453cb397732e07dcf5514152f
danmaiJJ <lidan9152@163.com>
2020-03-01 14:39:05 +0800

    删除老版本日志封装类

--- run_all.py
+++ run_all.py
@@ -2,15 +2,15 @@ import unittest
 from common.HTMLTestRunner_cn import HTMLTestRunner
 import os
 from common.sendEmail import SendEmail
-from common import log
+# from common import log
 
 root = os.getcwd()
 casePath = os.path.join(root, "test_case")  # 用例所在目录
 reportPath = os.path.join(root, "test_report", "result.html")  # 报告所在目录
 
-log = log.logger
-log.info('resultPath'+reportPath)#将resultPath的值输入到日志，方便定位查看问题
-log.info('caseList'+casePath)#同理
+# log = log.logger
+# log.info('resultPath'+reportPath)#将resultPath的值输入到日志，方便定位查看问题
+# log.info('caseList'+casePath)#同理
 
 # while 1 == 1:
 #     # if time.strftime("%Y-%m-%d %X") == '2019-08-09 08:11:30':
